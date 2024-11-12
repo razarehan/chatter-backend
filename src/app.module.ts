fbe1e2322faab42191c3ca54e277e7e32001ccba
rzzza_rehan <rhn.raza121@gmail.com>
2024-11-12 21:16:41 +0530

    Add validation and logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql'
 import * as Joi from 'joi';
 import { DatabaseModule } from './common/database/database.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UsersModule } from './users/users.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
@@ -21,9 +22,26 @@ import { UsersModule } from './users/users.module';
       autoSchemaFile: true
     }),
     DatabaseModule,
-    UsersModule
+    UsersModule,
+    LoggerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'production';
+        return {
+          pinoHttp: {
+            transport: isProduction ? undefined : {
+              target: 'pino-pretty',
+              options: {
+                singleLine: true
+              }
+            },
+            level: isProduction ? 'info' : 'debug'
+          }
+        }
+      },
+      inject: [ConfigService]
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
