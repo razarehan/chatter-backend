32aec61ec913a5af8c267dd148171d000b217ee5
rzzza_rehan <rhn.raza121@gmail.com>
2024-11-14 21:56:49 +0530

    change port to avoid clash with front-end

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
