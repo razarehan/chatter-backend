360a8ecf6d69e200de6c8174f25675a56215de09
rzzza_rehan <rhn.raza121@gmail.com>
2024-11-17 19:27:06 +0530

    jwt response handle here

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
+  app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
