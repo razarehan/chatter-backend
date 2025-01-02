8e849548d306f59c7f6ad37e68c07a4e7a87a6e5
rzzza_rehan <rhn.raza121@gmail.com>
2025-01-03 00:11:06 +0530

    get chats

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -14,8 +14,8 @@ export class ChatsService {
     });
   }
 
-  findAll() {
-    return `This action returns all chats`;
+  async findAll() {
+    return this.chatsRepository.find({});
   }
 
   findOne(id: number) {
