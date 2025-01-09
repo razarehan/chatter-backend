e223bdfca1d6e3ab5835f366c30a22e65259a40d
rzzza_rehan <rhn.raza121@gmail.com>
2025-01-09 21:30:25 +0530

    create message backend

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -1,14 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ChatsService } from './chats.service';
 import { ChatsResolver } from './chats.resolver';
 import { ChatsRepository } from './chats.repository';
 import { Chat, ChatSchema } from './entities/chat.entity';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]), // Important
+    MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
+    forwardRef(() => MessagesModule)
   ],
-  providers: [ChatsResolver, ChatsService, ChatsRepository]
+  providers: [ChatsResolver, ChatsService, ChatsRepository],
+  exports: [ChatsRepository]
 })
-export class ChatsModule {}
+export class ChatsModule { }
--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -10,7 +10,8 @@ export class ChatsService {
     return this.chatsRepository.create({
       ...createChatInput,
       userId,
-      userIds: createChatInput.userIds || []
+      userIds: createChatInput.userIds || [],
+      messages: []
     });
   }
 
